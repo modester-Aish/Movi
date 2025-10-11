16dbdd96ace125b1ef1100cceb39d372f3e15054
modester-Aish <developerw741@gmail.com>
2025-10-11 05:22:17 +0500

    Fix ESLint errors - prefer-const rule and unused directive warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,15 +19,16 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-    rules: {
-      // Disable all rules that cause build failures
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-      'react/no-unescaped-entities': 'off',
-      'react-hooks/exhaustive-deps': 'off',
-      '@next/next/no-img-element': 'off',
-    },
+           rules: {
+             // Disable all rules that cause build failures
+             '@typescript-eslint/no-explicit-any': 'off',
+             '@typescript-eslint/no-unused-vars': 'off',
+             '@typescript-eslint/no-require-imports': 'off',
+             'react/no-unescaped-entities': 'off',
+             'react-hooks/exhaustive-deps': 'off',
+             '@next/next/no-img-element': 'off',
+             'prefer-const': 'off',
+           },
   },
 ];
 
