4201c78cc73e3532ebdf7da090a2a2394a9518d8
modester-Aish <developerw741@gmail.com>
2025-10-13 00:15:43 +0500

    Remove 'Top IMDB' and 'Action Movies' categories from movies home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -349,9 +349,7 @@ export default function HomePage() {
   const movieCategoryConfig = [
     { name: "Suggestions", startIndex: 0, count: 14 },
     { name: "Trending Now", startIndex: 200, count: 14 },
-    { name: "Top Rated", startIndex: 300, count: 14 },
-    { name: "Top IMDB", startIndex: 400, count: 14 },
-    { name: "Action Movies", startIndex: 500, count: 14 }
+    { name: "Top Rated", startIndex: 300, count: 14 }
   ];
 
   const tvCategoryConfig = [
@@ -402,8 +400,6 @@ export default function HomePage() {
           "Suggestions": "suggestions",
           "Trending Now": "trending",
           "Top Rated": "top_rated",
-          "Top IMDB": "top_imdb",
-          "Action Movies": "action",
           "TV Shows": "tv_shows"
         };
         
@@ -444,8 +440,6 @@ export default function HomePage() {
                   "Suggestions": "suggestions",
                   "Trending Now": "trending",
                   "Top Rated": "top_rated",
-                  "Top IMDB": "top_imdb",
-                  "Action Movies": "action",
                   "TV Shows": "tv_shows"
                 };
         
@@ -596,16 +590,6 @@ export default function HomePage() {
                   case 'Top Rated': return (
                     <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
                       <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
-                    </svg>
-                  );
-                  case 'Top IMDB': return (
-                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
-                    </svg>
-                  );
-                  case 'Action Movies': return (
-                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/>
                     </svg>
                   );
                   default: return (
